9f107b524003a14095ae17f94be36e2ae2185c38
ZhouAndy <azhou34@my.bcit.ca>
2019-11-04 11:56:28 +0000

    Step 8

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,11 +1,11 @@
 
 #!/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS
+TRAVIS_REPO_SLUG_TOLOWER = $(echo "$TRAVIS_REPO_SLUG" | tr '[:upper:]' '[:lower:]')
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-TRAVIS_REPO_SLUG_TOLOWER = $(echo "$TRAVIS_REPO_SLUG" | tr '[:upper:]' '[:lower:]')
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG_TOLOWER:$TAG .
 docker push $TRAVIS_REPO_SLUG_TOLOWER:$TAG
